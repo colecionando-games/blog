b2ea3d3a26fdf56cd3344a713b2b5dded1897f32
Felipe B. Barbosa <felipebbarbosa@gmail.com>
2021-02-17 23:01:31 -0300

    Added support to Google Analytics (#7) and developed environments variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,91 @@
+require("dotenv").config()
+
+const pluginSetup = [
+  `gatsby-plugin-transition-link`,
+  `gatsby-plugin-styled-components`,
+  `gatsby-plugin-react-helmet`,
+  // needs to be the first to work with gatsby-remark-images
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `uploads`,
+      path: `${__dirname}/static/assets/img`,
+    }
+  },
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `posts`,
+      path: `${__dirname}/posts`,
+    }
+  },
+  `gatsby-transformer-sharp`,
+  `gatsby-plugin-sharp`,
+  {
+    resolve: `gatsby-transformer-remark`,
+    options: {
+      plugins: [
+        // this one is necessary when the images are outside of posts directory
+        {
+          resolve: `gatsby-remark-relative-images`,
+          options: {
+            name: `uploads`
+          }
+        },
+        { 
+          resolve: `gatsby-remark-images`,
+          options: {
+            maxWidth: 960,
+            linkImagesToOriginal: false
+          }
+        },
+        `gatsby-remark-lazy-load`,
+        `gatsby-remark-prismjs`
+      ]
+    }
+  },
+  {
+    resolve: `gatsby-plugin-google-fonts`,
+    options: {
+      fonts: [
+        `Roboto\:300,400,700`
+      ],
+      display: 'swap'
+    }
+  },
+  {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      name: `Colecionando.Games`,
+      short_name: `C.G Blog`,
+      start_url: `/`,
+      background_color: `#248bfd`,
+      theme_color: `#248bfd`,
+      display: `minimal-ui`,
+      icon: `static/assets/img/favicon.png`, // This path is relative to the root of the site.
+    },
+  },
+  `gatsby-plugin-sitemap`,
+  // this (optional) plugin enables Progressive Web App + Offline functionality
+  // To learn more, visit: https://gatsby.dev/offline
+  `gatsby-plugin-offline`,
+  `gatsby-plugin-netlify-cms`
+]
+
+if (process.env.CONTEXT === 'production') {
+  const analytics = {
+    resolve: `gatsby-plugin-google-gtag`,
+    options: {
+      trackingId: process.env.GOOGLE_ANALYTICS_ID,
+      head: false,
+      anonymize: true,
+      respectDNT: true
+    }
+  }
+
+  pluginSetup.push(analytics)
+}
+
 module.exports = {
   siteMetadata: {
     title: `Colecionando.Games`,
@@ -5,75 +93,5 @@ module.exports = {
     author: `Felipe B. Barbosa`,
     siteUrl: `https://blog.colecionando.games`
   },
-  plugins: [
-    `gatsby-plugin-transition-link`,
-    `gatsby-plugin-styled-components`,
-    `gatsby-plugin-react-helmet`,
-    // needs to be the first to work with gatsby-remark-images
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `uploads`,
-        path: `${__dirname}/static/assets/img`,
-      }
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `posts`,
-        path: `${__dirname}/posts`,
-      }
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          // this one is necessary when the images are outside of posts directory
-          {
-            resolve: `gatsby-remark-relative-images`,
-            options: {
-              name: `uploads`
-            }
-          },
-          { 
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 960,
-              linkImagesToOriginal: false
-            }
-          },
-          `gatsby-remark-lazy-load`,
-          `gatsby-remark-prismjs`
-        ]
-      }
-    },
-    {
-      resolve: `gatsby-plugin-google-fonts`,
-      options: {
-        fonts: [
-          `Roboto\:300,400,700`
-        ],
-        display: 'swap'
-      }
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Colecionando.Games`,
-        short_name: `C.G Blog`,
-        start_url: `/`,
-        background_color: `#248bfd`,
-        theme_color: `#248bfd`,
-        display: `minimal-ui`,
-        icon: `static/assets/img/favicon.png`, // This path is relative to the root of the site.
-      },
-    },
-    `gatsby-plugin-sitemap`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-netlify-cms`
-  ],
+  plugins: pluginSetup
 }
--- src/components/HeaderMenu/index.js
+++ src/components/HeaderMenu/index.js
@@ -1,22 +1,39 @@
 import React from "react"
+import ReactGA from "react-ga"
 
 import links from "./content"
 
 import * as S from "./styled"
 
 
-const HeaderMenu = () => (
-  <S.MenuLinksWrapper>
-    <S.MenuLinksList>
-      {links.map((link, i) => (
-        <S.MenuLinksItem key={i}>
-          <S.MenuLinksLink cover direction="left" bg="white" duration={0.6} to={link.url} activeClassName="active">
-            {link.label}
-          </S.MenuLinksLink>
-        </S.MenuLinksItem>
-      ))}
-    </S.MenuLinksList>
-  </S.MenuLinksWrapper>
-)
+const HeaderMenu = () => {
+  
+  const menuLinkClickTrack = link => {
+    ReactGA.event({
+      category: 'menu link',
+      action: 'click',
+      label: `Menu Link - ${link}`
+    })
+  }
+
+  return (
+    <S.MenuLinksWrapper>
+      <S.MenuLinksList>
+        {links.map((link, i) => (
+          <S.MenuLinksItem key={i}>
+            <S.MenuLinksLink cover direction="left" 
+              bg="white" duration={0.6} to={link.url} 
+              activeClassName="active"
+              onClick={() => menuLinkClickTrack(link.label)}
+            >
+              {link.label}
+            </S.MenuLinksLink>
+          </S.MenuLinksItem>
+        ))}
+      </S.MenuLinksList>
+    </S.MenuLinksWrapper>
+  )
+
+}
 
 export default HeaderMenu
