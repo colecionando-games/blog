2380aabf8f4f450fb2c6acd917bb4c2dd9d725ba
Felipe B. Barbosa <felipebbarbosa@gmail.com>
2021-03-15 21:27:59 -0300

    Added a logo at footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,26 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 
 import * as S from "./styled"
 
-const Footer = () => (
-  <S.FooterWrapper>
-    <span>Colecionando.Games está sendo desenvolvido por <a href="https://www.twitter.com/felipebbarbosa">Felipe B. Barbosa</a> e<br></br>pertence ao grupo <a href="https://www.vgscomcerveja.com.br">Videogames com Cerveja</a>.</span>
-  </S.FooterWrapper>
-)
+const Footer = () => {
+  const { logoFooter } = useStaticQuery(
+    graphql`
+      query {
+        logoFooter: file(relativePath: { eq: "footer_logo.png" }) {
+          childImageSharp {
+            gatsbyImageData(layout: FULL_WIDTH)
+          }
+        }
+      }`
+  )
+
+  return (
+    <S.FooterWrapper>
+      <S.FooterLogo image={logoFooter.childImageSharp.gatsbyImageData} alt="Logo Colecionado.Games"/>
+      <S.FooterCopyright>Colecionando.Games está sendo desenvolvido por <a href="https://www.twitter.com/felipebbarbosa">Felipe B. Barbosa</a> e pertence ao grupo <a href="https://www.vgscomcerveja.com.br">Videogames com Cerveja</a>.</S.FooterCopyright>
+    </S.FooterWrapper>
+  )
+}
 
 export default Footer
--- src/components/Footer/styled.js
+++ src/components/Footer/styled.js
@@ -1,4 +1,5 @@
 import styled from "styled-components"
+import { GatsbyImage } from "gatsby-plugin-image"
 
 export const FooterWrapper = styled.footer`
   height: auto;
@@ -6,11 +7,23 @@ export const FooterWrapper = styled.footer`
   background: rgb(17, 17, 17);
   color: white;
   font-size: 1.2em;
+  display: flex;
+  align-items: center;
   
 
   a {
     text-decoration: none;
     color: #2980b9;
   }
+`
+
+export const FooterCopyright = styled.span`
+  max-width: 400px;
+  margin-left: 10px;
+  line-height: 1.2em;
+`
+
+export const FooterLogo = styled(GatsbyImage)`
+  width: 80px;
 `
 
