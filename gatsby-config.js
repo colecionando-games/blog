bfe2cc733092a8df4788910d0eee6dbdbfda451e
Felipe B. Barbosa <felipebbarbosa@gmail.com>
2021-08-29 10:27:01 -0300

    fix: feed rss is now showing complete post

--- gatsby-config.js
+++ gatsby-config.js
@@ -121,6 +121,7 @@ const pluginSetup = [
                     description
                     date
                   }
+                  html
                   excerpt
                 }
               }
