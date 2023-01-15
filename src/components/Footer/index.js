2f0bfef5727fbce1c31ae038ab68f004349e3b0b
Felipe B. Barbosa <felipebbarbosa@gmail.com>
2023-01-15 11:52:15 -0300

    chore: update year of copyright on footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -23,7 +23,7 @@ const Footer = () => {
       <S.FooterCopyrightBox>
         <S.FooterLogo image={logoFooter.childImageSharp.gatsbyImageData} alt="Logo Colecionado.Games"/>  
         <S.FooterCopyright>
-          &copy; 2022 Colecionando.Games<br></br>
+          &copy; 2023 Colecionando.Games<br></br>
           <small>
             Um projeto independente sobre colecionismo e preservação de jogos eletrônicos.
           </small>
