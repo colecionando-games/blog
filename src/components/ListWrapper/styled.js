d7bfdba39ed39bf94b6693c2f83a453535902164
Felipe Barbosa <felipebbarbosa@gmail.com>
2024-09-01 10:12:05 -0300

    chore: list wrapper style adjust

--- src/components/ListWrapper/styled.js
+++ src/components/ListWrapper/styled.js
@@ -1,12 +1,17 @@
 import styled from "styled-components"
+import media from "styled-media-query"
 
 export const ListWrapper = styled.section`
   display: grid;
   grid-area: posts;
-  grid-gap: 20px;
+  grid-gap: 15px;
   grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
   max-width: 70rem;
   width: 100%;
   margin: 0 auto 20px;
-  padding: 0 10px;
+  padding: 0 20px;
+
+  ${media.lessThan("large")`
+    padding-top: 5px;
+  `}
 `
