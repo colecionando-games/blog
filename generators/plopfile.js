a98a84ebccb11a7b6a5c9f5d5d19ff08bb9197b7
Felipe Barbosa <felipebbarbosa@gmail.com>
2024-09-01 17:02:57 -0300

    chore: create a plop file to generate game database file

---
+++ generators/plopfile.js
@@ -0,0 +1,40 @@
+module.exports = function (plop) {
+  plop.setGenerator('game', {
+    description: 'application game database',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Digite o nome do jogo'
+      },
+      {
+        type: 'input',
+        name: 'releaseYear',
+        message: 'Ano de lançamento'
+      },
+      {
+        type: 'input',
+        name: 'developer',
+        message: 'Desenvolvido por'
+      },
+      {
+        type: 'list',
+        name: 'firstPlatform',
+        message: 'Escolha uma plataforma',
+        choices: [
+          'nes', 'snes', 'n64', 'gamecube', 'wii', 'wiiu', 'switch',
+          'gb', 'gbc', 'gba', 'nds', '3ds', 
+          'ps1', 'ps2', 'ps3', 'ps4', 'ps5', 'psp', 'ps-vita', 
+          'xbox-360', 'xbox-one', 'xbox-series'
+        ]
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: '../database/{{kebabCase name}}.json',
+        templateFile: 'templates/game.tsx.hbs'
+      }
+    ]
+  })
+}
