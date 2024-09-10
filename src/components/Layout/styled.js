00e4da01dce87d4068ab813bc21ec04f77be43e4
Felipe Barbosa <felipebbarbosa@gmail.com>
2024-09-09 21:10:20 -0300

    fix: layout style (issue #35)

--- src/components/Layout/styled.js
+++ src/components/Layout/styled.js
@@ -3,13 +3,11 @@ import styled from "styled-components"
 export const LayoutWrapper = styled.section`
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
+  min-height: 100vh;
 `
 
 export const LayoutMain = styled.main`
   width: 100%;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: stretch;
+  min-height: calc(100vh - 235px);
 `
