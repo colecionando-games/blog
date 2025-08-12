8f66ce45fa89dcdc54825f81cdc78b536fc506e0
Felipe B. Barbosa <felipebbarbosa@gmail.com>
2025-08-11 22:23:58 -0300

    chore: add about page

--- src/pages/sobre.js
+++ src/pages/sobre.js
@@ -19,31 +19,37 @@ const AboutPage = () => (
     <S.PostWrapper>
       <S.PostHeader>
         <S.PostTitle>Sobre o colecionando.games</S.PostTitle>
-        <S.PostDescription>Um projeto independente sobre colecionismo e preservação de jogos eletrônicos.</S.PostDescription>
+        <S.PostDescription>O colecionando.games é um projeto independente focado no colecionismo e na preservação de jogos eletrônicos.</S.PostDescription>
       </S.PostHeader>
 
       <S.MainContent>
         <p>
-          Nosso objetivo é reunir e preservar o máximo de informações possíveis sobre 
-          nosso hobby de colecionar videogames.
+          Nossa missão é reunir e preservar o máximo de informações possíveis sobre o 
+          fascinante universo de colecionar videogames.
         </p>
 
         <p>
-          Atualmente o projeto conta com este site que possui uma série de listas, 
-          artigos e notícias sobre a história e o colecionismo de games. 
-          O nosso <a href="https://www.instagram.com/colecionando.games">Instagram</a> é 
-          dedicado às versões brasileiras de jogos, valorizamos muito 
+          Atualmente o projeto conta com este site, que oferece uma variedade de listas, 
+          artigos e notícias sobre a história dos games e a cultura do colecionismo. 
+          Nosso perfil no <a href="https://www.instagram.com/colecionando.games">Instagram</a> é 
+          dedicado às versões brasileiras de jogos — valorizamos e celebramos tudo 
           o que é lançado por aqui. Também temos um canal 
-          no <a href="https://www.youtube.com/@colecionandogames">YouTube</a>, onde você 
-          encontra unboxings de jogos cujo o objetivo é registrar todo o conteúdo 
-          original do item para referência. 
-          E por fim, nosso perfil no <a href="https://archive.org/details/@felipebbarbosa">Internet Archive</a> com 
-          imagens (ISOs) de versões brasileiras de jogos, principalmente CD-ROMs da década de 90/00.
+          no <a href="https://www.youtube.com/@colecionandogames">YouTube</a> com unboxings 
+          detalhados, onde registramos o conteúdo original de cada item como referência. 
+          E, por fim, mantemos um acervo no <a href="https://archive.org/details/@felipebbarbosa">Internet Archive</a> com 
+          imagens (ISOs) de versões nacionais de jogos, principalmente em CD-ROMs da década de 1990 e 2000.
         </p>
 
-        <p>Ainda estamos apenas começando, mais novidades em breve!</p>
+        <p>Ainda estamos só no começo. Em breve, muitas novidades!</p>
 
-        
+        <h2>Quem faz?</h2>
+
+        <h4>Felipe B. Barbosa</h4>
+
+        <p>
+          Desenvolvedor de software metido a colecionador, historiador e arquivista de jogos eletrônicos. 
+          Além de ter criado este site, também escreve para o <a href="https://www.vgscomcerveja.com.br" target="_blank">Videogames com Cerveja</a>.
+        </p>
         
         
         
