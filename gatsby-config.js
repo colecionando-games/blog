8e62ac0d96ee195c589c8c9f0fb99ac42d2b29d6
Felipe B. Barbosa <felipebbarbosa@gmail.com>
2025-10-13 20:55:15 -0300

    chore: remove netlify plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -83,17 +83,6 @@ const pluginSetup = [
     }
   },
   `gatsby-plugin-sitemap`,
-  {
-    resolve: `gatsby-plugin-netlify`,
-    options: {
-      headers: {},
-      allPageHeaders: [],
-      mergeSecurityHeaders: true,
-      mergeCachingHeaders: true,
-      transformHeaders: (headers, path) => headers,
-      generateMatchPathRewrites: true
-    }
-  },
   // this (optional) plugin enables Progressive Web App + Offline functionality
   // To learn more, visit: https://gatsby.dev/offline
   //`gatsby-plugin-offline`,
