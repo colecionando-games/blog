37c76397609143df5f063aadaaa0519f35898bab
Felipe B. Barbosa <felipebbarbosa@gmail.com>
2026-08-22 08:01:41 -0300

    chore: add tag referrer on head

--- src/html.js
+++ src/html.js
@@ -11,6 +11,7 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
+        <meta name="referrer" content="strict-origin-when-cross-origin" />
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
